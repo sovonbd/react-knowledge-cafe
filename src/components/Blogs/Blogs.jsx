977773914519feb3b8a808ae56fa66367b5a9512
sovonbd <sovonsdp@gmail.com>
2023-09-11 11:53:07 -0500

    responsive layout created

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -8,7 +8,11 @@ const Blogs = () => {
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
-  return <div></div>;
+  return (
+    <div className="w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+    </div>
+  );
 };
 
 export default Blogs;
